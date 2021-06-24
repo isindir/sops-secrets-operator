4a06cd8fe0fb09c86ae5191dbe104d6c2334a017
Eriks Zelenka <isindir@users.sf.net>
2021-06-24 06:45:53 +0100

    Add CRD support for multiple versions

---
+++ api/v1alpha1/sopssecret_types.go
@@ -0,0 +1,52 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// SopsSecretSpec defines the desired state of SopsSecret
+type SopsSecretSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of SopsSecret. Edit sopssecret_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// SopsSecretStatus defines the observed state of SopsSecret
+type SopsSecretStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// SopsSecret is the Schema for the sopssecrets API
+type SopsSecret struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   SopsSecretSpec   `json:"spec,omitempty"`
+	Status SopsSecretStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// SopsSecretList contains a list of SopsSecret
+type SopsSecretList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []SopsSecret `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&SopsSecret{}, &SopsSecretList{})
+}
