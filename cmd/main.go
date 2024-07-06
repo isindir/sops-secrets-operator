3ee0d00792475bc86d6f6ca0dc25d67e13020267
Eriks Zelenka <isindir@users.sf.net>
2024-07-06 18:18:22 +0100

    Fix typo

--- cmd/main.go
+++ cmd/main.go
@@ -52,7 +52,7 @@ func main() {
 	var requeueAfter int64
 	var watchNamespace string
 
-	flag.StringVar(&metricsAddr, "metrics-bind-address", metricsAddr, "The address the metric endpoint binds to.")
+	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
